0ff2d528ee56f68444c111c6286bb0eabb6dee06
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-07-26 15:47:06 +0800

    clean up unused code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -64,10 +64,10 @@ angular.module('starter.controllers', [])
 		$scope.login.show();
 	};
 	$scope.authPopup = function () {
-		auth.signInWithRedirect(provider);
+		firebase.auth().signInWithRedirect(provider);
 	};
 	$scope.signOut = function () {
-		auth.signOut();
+		firebase.auth().signOut();
 		window.localStorage['hideLogin'] = false;
 		$rootScope.userSignedIn = false;
 		$rootScope.hideLogin = false;
@@ -84,7 +84,7 @@ angular.module('starter.controllers', [])
 	};
 
 	firebase.auth().getRedirectResult().then(function(result) {
-		userData.setUser(auth.currentUser);
+		userData.setUser(firebase.auth().currentUser);
 	}).catch(function(error) {
 	});
 	// ---------------------------------------------------------------------------------
@@ -338,68 +338,6 @@ angular.module('starter.controllers', [])
 	};
 	tick();
 	$interval(tick, 1000);
-
-
-	//Payment UI
-
-	// var card = elements.create('card', {
-	//   iconStyle: 'solid',
-	//   style: {
-	//     base: {
-	//       iconColor: '#8898AA',
-	//       color: 'white',
-	//       lineHeight: '36px',
-	//       fontWeight: 300,
-	//       fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-	//       fontSize: '19px',
-
-	//       '::placeholder': {
-	//         color: '#8898AA',
-	//       },
-	//     },
-	//     invalid: {
-	//       iconColor: '#e85746',
-	//       color: '#e85746',
-	//     }
-	//   },
-	//   classes: {
-	//     focus: 'is-focused',
-	//     empty: 'is-empty',
-	//   },
-	// });
-	// card.mount('#card-element');
-
-	// function setOutcome(result) {
-	//   var successElement = document.querySelector('.success');
-	//   var errorElement = document.querySelector('.error');
-	//   successElement.classList.remove('visible');
-	//   errorElement.classList.remove('visible');
-
-	//   if (result.token) {
-	//     // Use the token to create a charge or a customer
-	//     // https://stripe.com/docs/charges
-	//     successElement.querySelector('.token').textContent = result.token.id;
-	//     successElement.classList.add('visible');
-	//   } else if (result.error) {
-	//     errorElement.textContent = result.error.message;
-	//     errorElement.classList.add('visible');
-	//   }
-	// }
-
-	// card.on('change', function(event) {
-	//   setOutcome(event);
-	// });
-
-	// document.querySelector('form').addEventListener('submit', function(e) {
-	//   e.preventDefault();
-	//   var form = document.querySelector('form');
-	//   var extraDetails = {
-	//     name: form.querySelector('input[name=cardholder-name]').value,
-	//   };
-	//   stripe.createToken(card, extraDetails).then(setOutcome);
-	// });
-	//----------------------------------------
-
 
 	//Payment
   $http.defaults.headers.common['X-Mashape-Key']  = NOODLIO_PAY_API_KEY;
