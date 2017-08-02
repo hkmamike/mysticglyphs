aa7a7f57a2e15871163da3d874d2356b502dad13
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-08-02 19:02:26 +0800

    added minification workflow and removed unsued files

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,16 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssMin = require('gulp-css');
+var ngAnnotate = require('gulp-ng-annotate');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
-
 var paths = {
   sass: ['./scss/**/*.scss']
 };
-
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
 
 gulp.task('watch', ['sass'], function() {
   gulp.watch(paths.sass, ['sass']);
@@ -49,3 +36,37 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('css', function() {
+
+  gulp.src(['./sourcewww/css/style.css','./lib/ionic/css/ionic.css'])
+  .pipe(concat('style.min.css'))
+  .pipe(cssMin())
+  .pipe(gulp.dest('./www/css/'));
+
+});
+
+gulp.task('scripts', function() {
+
+  gulp.src(['./sourcewww/js/app.js', './sourcewww/js/controllers.js', './sourcewww/js/DatabaseUploader.js'])
+  .pipe(concat('app.min.js'))
+  .pipe(ngAnnotate())
+  .pipe(uglify())
+  .pipe(gulp.dest('./www/js/'));
+
+});
+
+gulp.task('default', ['sass', 'css', 'scripts']);
---
+++ sourcewww/js/app.js
@@ -0,0 +1,104 @@
+//Payment API variables
+var NOODLIO_PAY_API_URL         = "https://noodlio-pay.p.mashape.com";
+var NOODLIO_PAY_API_KEY         = "KWM88xJZ4fmshN1FqiBLznS8Vn0vp1Oc0CSjsnYuBNgnk5wUHa";
+var STRIPE_ACCOUNT_ID           = "acct_1A9Z68HnE9ndDvpX";
+// development mode (TEST_MODE: true) or production mode (TEST_MODE: false)
+var TEST_MODE = true;
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.DatabaseUploader', 'firebase'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    } if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.run(['$rootScope', '$state', function($rootScope, $state){
+  $rootScope.$on('$stateChangeStart',function(){
+      $rootScope.stateIsLoading = true;
+  });
+  $rootScope.$on('$stateChangeSuccess',function(){
+    $rootScope.stateIsLoading = false;
+  });
+}])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  //Update Database
+  .state('updateDatabase', {
+    url: '/updatedatabase',
+    templateUrl: 'templates/updatedatabase.html',
+    controller: 'TempCtrl'
+  })
+  //Intro Page
+  .state('intro', {
+    url: '/intro',
+    templateUrl: 'templates/intro.html',
+    controller: 'AppCtrl'
+  })
+  //App Abstract
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  //Cities List
+  .state('app.List', {
+    url: '/List',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/List.html'
+      }
+    }
+  })
+  //Mission List in City
+  .state('app.City', {
+    url: '/List/City/:CityID',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/City.html',
+        controller: 'ListCtrl'
+      }
+    }
+  })
+  //MissionInfo Info
+  .state('app.MissionInfo', {
+    url: '/List/City/:CityID/MissionInfo/:MissionID',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/MissionInfo.html',
+        controller: 'MapCtrl'
+      }
+    }
+  })
+  //Mission Page
+  .state('app.Mission', {
+    url: '/List/City/:CityID/Mission/:MissionID',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/Mission.html',
+        controller: 'ListCtrl'
+      }
+    }
+  })
+  //Map Page
+  .state('app.map', {
+    url: '/List/City/:CityID/Map/:MissionID',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/mapPage.html',
+        controller: 'MapCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/intro');
+});
