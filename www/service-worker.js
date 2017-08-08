24b14955328f49b6e6b573fc3c4437f45128f346
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-08-08 17:43:35 +0800

    working on service worker registration

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,3 +1,22 @@
+self.addEventListener('install', function(event){
+  // only happens once for this version of the service worker
+  // wait until the install event has resolved
+  // event.waitUntil(
+  //   // then create our named cached
+  //   caches
+  //   .open('my-sw-cache')
+  //   .then(function(cache) {
+  //     // once created, lets add some local resouces
+  //     return cache.addAll([
+  //       './js/app.min.js',
+  //       './css/style.min.css'
+  //     ]);
+  //   })
+  //   .then(function(){
+  //     console.log('Service worker is ready, and assets are cached');
+  //   });
+  // );
+});
 
 self.addEventListener('activate', function (event) {
 
@@ -9,24 +28,4 @@ self.addEventListener('fetch', function (event) {
 
 self.addEventListener('push', function (event) {
 
-});
-
-self.addEventListener('install', function(event){
-  // only happens once for this version of the service worker
-  // wait until the install event has resolved
-  event.waitUntil(
-    // then create our named cached
-    caches
-    .open('my-sw-cache')
-    .then(function(cache) {
-      // once created, lets add some local resouces
-      return cache.addAll([
-        './js/app.min.js',
-        './css/style.min.css'
-      ]);
-    })
-    .then(function(){
-      console.log('Service worker is ready, and assets are cached');
-    });
-  );
 });
